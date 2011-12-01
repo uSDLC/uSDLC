c0acb980f8726c3947881064d027f1fa295de214
Paul Marrington <paul@marrington.net>
2011-12-01 19:35:51 +1000

    implement projects

---
+++ web/contentTree_html.groovy
@@ -0,0 +1,11 @@
+package usdlc
+
+Store.base('~/').file.eachDir { File file ->
+	if (new File("$file.absolutePath/usdlc").exists()) {
+		def href = "~/$file.name/usdlc/index.html"
+		def project = file.name.capitalize().replaceAll(~/[\W_\$]/, ' ')
+		write "<a href='$href' class='usdlc' action='page'>$project<a/>\n"
+	}
+}
+
+write Store.base('frontPage.html').text
