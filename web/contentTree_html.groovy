7549e0446e8a910d853ec710cd0441d2ba7acb35
Paul Marrington <paul@marrington.net>
2012-01-08 11:46:00 +1000

    usdlc-30: Separate words for projects

--- web/contentTree_html.groovy
+++ web/contentTree_html.groovy
@@ -4,7 +4,7 @@ Store.base('~/').file.eachDir { File file ->
 	if (file.name.toLowerCase() != 'usdlc' &&
 	  new File("$file.absolutePath/usdlc").exists()) {
 		def href = "~/$file.name/usdlc/index.html"
-		def project = file.name.capitalize().replaceAll(~/[\W_\$]/, ' ')
+		def project = Store.decamel(file.name.capitalize())
 		write "<a href='$href' class='usdlc' action='page'>$project<a/>\n"
 	}
 }
