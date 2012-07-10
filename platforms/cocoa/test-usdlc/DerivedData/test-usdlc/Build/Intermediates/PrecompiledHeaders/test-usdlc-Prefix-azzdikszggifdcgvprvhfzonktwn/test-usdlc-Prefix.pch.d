88ea286ea249bf34cda798b5221aef736fde7a85
Paul Marrington <paul@marrington.net>
2012-07-10 20:05:37 +1000

    removing unwanted

--- platforms/cocoa/test-usdlc/DerivedData/test-usdlc/Build/Intermediates/PrecompiledHeaders/test-usdlc-Prefix-azzdikszggifdcgvprvhfzonktwn/test-usdlc-Prefix.pch.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/paul/Dropbox/My\ Cabinets/Development/uSDLC/platforms/cocoa/test-usdlc/test-usdlc/test-usdlc-Prefix.pch
--- platforms/cocoa/test-usdlc/DerivedData/test-usdlc/Build/Intermediates/test-usdlc.build/Debug-iphonesimulator/test-usdlc.build/Objects-normal/i386/DetailViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/paul/Dropbox/My\ Cabinets/Development/uSDLC/platforms/cocoa/test-usdlc/test-usdlc/DetailViewController.m \
-  /Users/paul/Dropbox/My\ Cabinets/Development/uSDLC/platforms/cocoa/test-usdlc/test-usdlc/DetailViewController.h
