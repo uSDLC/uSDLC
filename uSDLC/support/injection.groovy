79f28d6b3b499e8b19c1864a02f1825ce26221c6
Justin Marrington <justin@marrington.net>
2012-12-18 12:53:24 +1000

    Injection tests for Java-based devices.

--- uSDLC/support/injection.groovy
+++ uSDLC/support/injection.groovy
@@ -5,9 +5,11 @@ result = exchange.request.query.result
 remote = RemoteComms.get(device)
 
 if (result) {
+    println "[ $result ]"
     println "Test returned $result"
     remote.toUSDLC.put(result)
 } else {
+    println exchange.request.query
     println "Connected to ${device}, waiting for code..."
     code = remote.fromUSDLC.take()
     println "Injecting code into ${device}"
